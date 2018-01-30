871df53648f0d5b67b1cddec0679246ec17d7377
KyoungAh <kyoungah@kyoungah.com>
2018-01-31 01:23:15 +0900

    react, webpack 설치 및 초기 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-es2015', '@babel/preset-react']
+          }              
+        }
+      }
+    ]
+  }
 }
